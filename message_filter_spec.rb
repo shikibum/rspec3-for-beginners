8409127063819e3940310ecf62a242627e72e3c3
shikibum <satoshikibu@gmail.com>
2018-09-18 15:44:36 +0900

    Add first spec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,4 +1,8 @@
 require_relative 'message_filter' # require_relativeは相対パス
 
 describe MessageFilter do
+  it 'detects message with NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to  eq true
+  end
 end
