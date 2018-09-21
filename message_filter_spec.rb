5bbec452f98608a4f86e0b50cc94f8fc2795eced
shikibum <satoshikibu@gmail.com>
2018-09-21 15:13:27 +0900

    Add example for ng_words size

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,17 +4,18 @@ describe MessageFilter do
   shared_examples 'MessageFilter with argument "foo"' do
     it { is_expected.to be_detect('hello from foo') }
     it { is_expected.not_to be_detect('hello, world!') }
+    it { expect(subject.ng_words).not_to be_empty }
   end
 
-  describe 'with argument "foo"' do
+  context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
-    it 'ng_words should not be empty' do
-      expect(subject.ng_words).not_to be_empty
+    it 'ng_words size is 1' do
+      expect(subject.ng_words.size).to eq 1
     end
   end
 
-  describe 'with argument "foo","bar"' do
+  context 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo', 'bar') }
   it { is_expected.to be_detect('hello from bar') }
   it_behaves_like 'MessageFilter with argument "foo"'
