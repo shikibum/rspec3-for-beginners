acd637582144e5fd31d3419331d8d98172a5b921
shikibum <satoshikibu@gmail.com>
2018-09-21 13:57:25 +0900

    Use predicate matcher

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -5,10 +5,15 @@ describe MessageFilter do
     it { is_expected.to be_detect('hello from foo') }
     it { is_expected.not_to be_detect('hello, world!') }
   end
+
   describe 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
+    it 'ng_words should not be empty' do
+      expect(subject.ng_words).not_to be_empty
+    end
   end
+
   describe 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo', 'bar') }
   it { is_expected.to be_detect('hello from bar') }
